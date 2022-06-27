5773da8b0f7029bf782cc4cefd0057749d4a840e
Eyüphan Akkaya <96181594+eyuphanakkaya@users.noreply.github.com>
2022-06-27 14:23:50 +0300

    ReCapCar

---
+++ ReCapProject/Business/Concrete/CarManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+
+        ICar _car;
+
+        public CarManager(ICar car)
+        {
+            _car = car;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _car.GetAll();
+        }
+    }
+}
---
+++ ReCapProject/ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.CarId);
+                Console.WriteLine(car.BrandId);
+                Console.WriteLine(car.ColorId);
+                Console.WriteLine(car.DailyPrice);
+                Console.WriteLine(car.Description);
+            }
+        }
+    }
+}
